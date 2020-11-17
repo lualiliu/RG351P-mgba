52500078ba97c6a5633fab5971f7870cc63ecdc4
Christian Haitian <christianhaitian0@gmail.com>
2020-11-17 03:35:43 +0000

    Added rumble support for Anbernic RG351P

--- src/platform/libretro/libretro.c
+++ src/platform/libretro/libretro.c
@@ -1504,9 +1504,11 @@ void retro_run(void) {
 
 	if (rumbleCallback) {
 		if (rumbleUp) {
+			system("echo 10 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle");
 			rumbleCallback(0, RETRO_RUMBLE_STRONG, rumbleUp * 0xFFFF / (rumbleUp + rumbleDown));
 			rumbleCallback(0, RETRO_RUMBLE_WEAK, rumbleUp * 0xFFFF / (rumbleUp + rumbleDown));
 		} else {
+			system("echo 1000000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle");
 			rumbleCallback(0, RETRO_RUMBLE_STRONG, 0);
 			rumbleCallback(0, RETRO_RUMBLE_WEAK, 0);
 		}
